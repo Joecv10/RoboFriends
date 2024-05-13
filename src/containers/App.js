08f8d3ce0367d5ec359443928fed578601d4e105
Joe Concha Vasquez <joeconchavasquez@gmail.com>
2024-05-12 23:02:52 -0500

    First part of transforming the App cpmponent into hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import ErrorBoundry from "../components/ErrorBoundry";
 import "./App.css";
 
-class App extends React.Component {
+const App = () => {
+  const onSearch = (event) => {
+    setSearchField(event.target.value);
+  };
+  const [robots, setRobots] = useState([]);
+  const [searchField, setSearchField] = useState("");
+
+  const filteredRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchField.toLowerCase());
+  });
+
+  if (!robots.length) {
+    return <h1>Loading</h1>;
+  } else {
+    return (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={onSearch} />
+        <Scroll>
+          <ErrorBoundry>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundry>
+        </Scroll>
+      </div>
+    );
+  }
+};
+
+/*class App extends React.Component {
   constructor() {
     super();
     this.state = {
@@ -46,6 +74,6 @@ class App extends React.Component {
       );
     }
   }
-}
+}*/
 
 export default App;
